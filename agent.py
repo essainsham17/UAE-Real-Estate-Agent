0895dd20e60001a7d75c18dc544ff54643f7864b
Essa Insham <essainsham17@gmail.com>
2026-05-18 14:22:05 +0400

    Upgrade to 2.0: added pydantic and changed dataset

--- agent.py
+++ agent.py
@@ -6,64 +6,98 @@ from langchain_groq import ChatGroq
 import joblib
 from langgraph.graph import StateGraph, START,END
 from dotenv import load_dotenv
+from pydantic import BaseModel , Field
 
 load_dotenv()
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
 llm=ChatGroq(model='llama-3.3-70b-versatile', temperature=0)
+prediction_model=joblib.load("price_prediction_model.pkl")
+columns=joblib.load("model_columns.pkl")
+
+class PropertyExtraction(BaseModel):
+    Beds: int = Field(
+        default=1, description="the number of bedrooms. set the default to 1 if user did'nt specify"
+    )
+    Baths: int = Field(
+        description="the number of bathrooms. if not specified assume bedrooms+1"
+    )
+    Type: str =Field(
+        default='Apartment',description="the type of property (eg: Villa,Apartment). default set to Apartment if not specified"
+    )
+    Area_in_sqft: float = Field(
+        description="Total area in square feet. If not explicitly mentioned, estimate it by multiplying the number of Bedrooms by 750, plus 24."
+    )
+    Furnishing: str = Field(
+        default="Unfurnished", 
+        description="Furnishing status (Furnished or Unfurnished). Default to 'Unfurnished'."
+    )
+    Location: str = Field(
+        description="the exact location of the property. if not mensioned return 'UNKNOWN'"
+    )
+    City: str = Field(
+        default="Dubai", 
+        description="The city. Default to 'Dubai'."
+    )
 
 class AgentState(TypedDict):
     user_query:str
+
+    
+    Beds: int
+    Baths: int
+    Type: str
+    Area_in_sqft: float
+    Furnishing: str
     Location:str
-    Bedrooms:int
+    City: str
+
+    
     predicted_price:float
     final_response:str
-
     down_payment:float
     monthly_payment:float
 
 
 def extract_property_info(state: AgentState):
     text=state["user_query"]
-    prompt=f"""
-    you are a data extraction bot.
-    extract the location and the number of bedrooms from this user query:"{text}".
-    Rules:
-    1. return only comma seperated string
-    2. format: Location, Bedrooms
-    3. do NOT add any conversational text
-    4. Example: JVC, 2
-    """
+    
+    structured_llm=llm.with_structured_output(PropertyExtraction)
+    extracted_content=structured_llm.invoke(text)
+    
+    return (
+        {
+        'Beds':extracted_content.Beds,
+        'Baths':extracted_content.Baths,
+        'Type':extracted_content.Type,
+        'Area_in_sqft':extracted_content.Area_in_sqft,
+        'Furnishing':extracted_content.Furnishing,
+        'Location':extracted_content.Location,
+        'City':extracted_content.City
+        }
+    )
 
-    response=llm.invoke(prompt)
-    extracted_content=response.content
-
-    try:
-        parts=extracted_content.split(',')
-        location=parts[0].strip()
-        n_bedrooms=int(parts[1].strip())
-    except Exception as e:
-        location= "Unknown"
-        n_bedrooms=0
-
-    return({'Location':location,'Bedrooms':n_bedrooms})
+    
 def Predictor(state: AgentState):
-    location=state["Location"]
-    n_bedrooms=state["Bedrooms"]
-
-    prediction_model=joblib.load("uae_real_estate_model.pkl")
-    columns=joblib.load("model_columns.pkl")
-
-    df=pd.DataFrame({col: 0 for col in columns},index=[0])
-    if location in columns:
-        df[location]=1
-    df['Area(Sqft)']=(n_bedrooms*750)+24
-    df['Bedrooms']=n_bedrooms
+    input_data={
+        'Beds':[state['Beds']],
+        'Baths':[state['Baths']],
+        'Type':[state['Type']],
+        'Area_in_sqft':[state['Area_in_sqft']],
+        'Furnishing':[state['Furnishing']],
+        'Location':[state['Location']],
+        'City':[state['City']]
+    }
+    
+    df=pd.DataFrame(input_data)
+    
     
     result=prediction_model.predict(df)
-    final_result=round(result[0],2)
+    final_result=np.exp(result[0])
 
     return {'predicted_price':final_result}
+
+
 def Mortgage_calculator(state: AgentState):
     price=state['predicted_price']
     loan=price*0.8
@@ -76,7 +110,7 @@ def Mortgage_calculator(state: AgentState):
 def Generate_Response(state: AgentState):
     query=state['user_query']
     location=state['Location']
-    bedrooms=state['Bedrooms']
+    Beds=state['Beds']
     price=state['predicted_price']
     down_payment=state['down_payment']
     monthly_payment=state['monthly_payment']
@@ -95,15 +129,16 @@ def Generate_Response(state: AgentState):
 workflow=StateGraph(AgentState)
 
 workflow.add_node("extractor",extract_property_info)
-workflow.add_node("perdictor",Predictor)
+workflow.add_node("predictor",Predictor)
 workflow.add_node("responder",Generate_Response)
 workflow.add_node('Mortgage',Mortgage_calculator)
 
 workflow.add_edge(START,"extractor")
-workflow.add_edge("extractor","perdictor")
-workflow.add_edge("perdictor","Mortgage")
+workflow.add_edge("extractor","predictor")
+workflow.add_edge("predictor","Mortgage")
 workflow.add_edge("Mortgage","responder")
-workflow.add_edge('Mortgage',END)
+workflow.add_edge('responder',END)
 
 real_estate_agent=workflow.compile()
+
 
