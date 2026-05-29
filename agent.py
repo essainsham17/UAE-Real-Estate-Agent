f76a93a133ffd176756bea395ab009ef916d6a2d
Essa Insham <essainsham17@gmail.com>
2026-05-29 17:06:58 +0400

    Version 3

--- agent.py
+++ agent.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
-from typing import TypedDict
+from typing import TypedDict, Optional
 import os
 from langchain_groq import ChatGroq
 import joblib
 from langgraph.graph import StateGraph, START,END
 from dotenv import load_dotenv
 from pydantic import BaseModel , Field
+from langgraph.checkpoint.memory import MemorySaver
 
+memory=MemorySaver()
 load_dotenv()
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
@@ -16,24 +18,27 @@ prediction_model=joblib.load("price_prediction_model.pkl")
 columns=joblib.load("model_columns.pkl")
 
 class PropertyExtraction(BaseModel):
-    Beds: int = Field(
-        default=1, description="the number of bedrooms. set the default to 1 if user did'nt specify"
+    Beds: Optional[int] = Field(
+        default=None,
+        description="the number of bedrooms. set the default to None if user did'nt specify"
     )
     Baths: int = Field(
-        description="the number of bathrooms. if not specified assume bedrooms+1"
+        default=2, description="the number of bathrooms. if not specified assume bedrooms+1"
     )
-    Type: str =Field(
-        default='Apartment',description="the type of property (eg: Villa,Apartment). default set to Apartment if not specified"
+    Type: Optional[str] =Field(
+        default=None,
+        description="the type of property (eg: Villa,Apartment). default set to None if not specified"
     )
     Area_in_sqft: float = Field(
-        description="Total area in square feet. If not explicitly mentioned, estimate it by multiplying the number of Bedrooms by 750, plus 24."
+        default=750, description="Total area in square feet. If not explicitly mentioned, estimate it by multiplying the number of Bedrooms by 750, plus 24."
     )
-    Furnishing: str = Field(
-        default="Unfurnished", 
-        description="Furnishing status (Furnished or Unfurnished). Default to 'Unfurnished'."
+    Furnishing: Optional[str] = Field(
+        default=None,
+        description="Furnishing status (Furnished or Unfurnished). Default to None."
     )
-    Location: str = Field(
-        description="the exact location of the property. if not mensioned return 'UNKNOWN'"
+    Location: Optional[str] = Field(
+        default=None,
+        description="the exact location of the property. if not mentioned return None"
     )
     City: str = Field(
         default="Dubai", 
@@ -42,6 +47,7 @@ class PropertyExtraction(BaseModel):
 
 class AgentState(TypedDict):
     user_query:str
+    validation_status: str
 
     
     Beds: int
@@ -55,15 +61,30 @@ class AgentState(TypedDict):
     
     predicted_price:float
     final_response:str
-    down_payment:float
-    monthly_payment:float
+    monthly_rent:float
+    annual_rent:float
+    security_deposit: float
+    agent_commission: float
 
 
 def extract_property_info(state: AgentState):
     text=state["user_query"]
-    
+    Beds=state.get('Beds')
+    Baths=state.get('Baths')
+    Type=state.get('Type')
+    Area_in_sqft=state.get('Area_in_sqft')
+    Furnishing=state.get('Furnishing')
+    Location=state.get('Location')
+    City=state.get('City')
+
+    context=f"""
+    existing property details are: Beds={Beds}, Baths={Baths}, Type={Type}, Area_in_sqft={Area_in_sqft}, 
+    Furnishing={Furnishing}, Location={Location}, City={City}
+    new user message= {text}
+    Only update fields explicitly mentioned in the new message. Keep everything else the same.
+    """
     structured_llm=llm.with_structured_output(PropertyExtraction)
-    extracted_content=structured_llm.invoke(text)
+    extracted_content=structured_llm.invoke(context)
     
     return (
         {
@@ -78,6 +99,26 @@ def extract_property_info(state: AgentState):
     )
 
     
+def validate_inputs(state: AgentState):
+    Beds=state.get('Beds')
+    Type=state.get('Type')
+    Furnishing=state.get('Furnishing')
+    Location=state.get('Location')
+
+
+    valid_list=[
+        (Beds, 'How many bedroom are you looking for?'),
+        (Type,"What type of Property are you looking for: choose from 'Apartment, Townhouse or Villa'"),
+        (Furnishing,'Are you looking for Furnished or Unfurnished?'),
+        (Location,"Where do you want your Property.?")
+    ]
+    for value,question in valid_list:
+        if value == None:
+            return {'validation_status':'Incomplete', 'final_response':question}
+    else:
+        return {'validation_status':'Complete'}
+
+
 def Predictor(state: AgentState):
     input_data={
         'Beds':[state['Beds']],
@@ -93,52 +134,65 @@ def Predictor(state: AgentState):
     
     
     result=prediction_model.predict(df)
-    final_result=np.exp(result[0])
+    final_result=float(np.exp(result[0]))
 
     return {'predicted_price':final_result}
 
 
-def Mortgage_calculator(state: AgentState):
-    price=state['predicted_price']
-    loan=price*0.8
-    interest=0.045/12
-    months=25*12
+def Rental_calculator(state: AgentState):
+    Monthly_rent=state['predicted_price']
     
-    EMI=round(loan*(interest*(interest+1)**months)/((interest+1)**months-1),2)
+    Annual_rent=Monthly_rent*12
+    security_deposit=Annual_rent*0.05
+    agent_commission=Annual_rent*0.02
+    
+    return {"monthly_rent":Monthly_rent,'annual_rent':Annual_rent, 'security_deposit':security_deposit, 'agent_commission': agent_commission}
 
-    return {"down_payment":(price*0.2),"monthly_payment":EMI}
+
+
 def Generate_Response(state: AgentState):
     query=state['user_query']
     location=state['Location']
-    Beds=state['Beds']
     price=state['predicted_price']
-    down_payment=state['down_payment']
-    monthly_payment=state['monthly_payment']
+    monthly_rent=state['monthly_rent']
+    annual_rent=state['annual_rent']
+    security_deposit=state['security_deposit']
+    agent_commission=state['agent_commission']
 
-    prompt=f'''
-    you are a UAE real estate AI. generate a professional response to the users query:{query}.
-    Compare the users expected price in the query with the actual model predicted price = {price}.
-    also consider the location {location}.
-    Also give the client about the down payment of the property is {down_payment} which is 20% of the total price.
-    Monthly payment for 25 years with 4.5% interest rate is calculated at {monthly_payment}'''
+
+    prompt=f""" you are a uae real estate rental advisor.respond in warm, helpfull and professional tone to  generate a professional response to the users query: {query}.
+    compare the users expected price in the query with the actual model predicted price = {price}.
+    also consider the location: {location}.
+    also give the client information about monthly rent {monthly_rent}, annual rent:{annual_rent}, security_deposit of 5% of annual rent is: {security_deposit}
+and agent commission is 2% of annual rent is {agent_commission}.
+Also inform the client that the total upfront cost including security deposit and agent commission is {security_deposit + agent_commission}
+    """
 
     response=llm.invoke(prompt)
 
     return {'final_response':response.content}
+
+def rout_validator(state: AgentState):
+    if state.get('validation_status') == 'Incomplete':
+        return(END)
+    if state.get('validation_status') == 'Complete':
+        return('predictor')
 
 workflow=StateGraph(AgentState)
 
 workflow.add_node("extractor",extract_property_info)
 workflow.add_node("predictor",Predictor)
 workflow.add_node("responder",Generate_Response)
-workflow.add_node('Mortgage',Mortgage_calculator)
+workflow.add_node('Rental',Rental_calculator)
+workflow.add_node('Validator', validate_inputs)
 
 workflow.add_edge(START,"extractor")
-workflow.add_edge("extractor","predictor")
-workflow.add_edge("predictor","Mortgage")
-workflow.add_edge("Mortgage","responder")
+workflow.add_edge("extractor","Validator")
+workflow.add_conditional_edges("Validator",rout_validator)
+workflow.add_edge("predictor","Rental")
+workflow.add_edge("Rental","responder")
 workflow.add_edge('responder',END)
 
-real_estate_agent=workflow.compile()
+real_estate_agent=workflow.compile(checkpointer=memory)
 
 
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from agent import real_estate_agent
+import uuid
 
 st.set_page_config('UAE Real Estate AI', page_icon='🏘️')
 
@@ -8,6 +9,9 @@ st.markdown("Your intelligent guide to Dubai and Abu Dhabi property valuations."
 
 if "messages" not in st.session_state:
     st.session_state.messages=[]
+
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id=str(uuid.uuid4())
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -19,7 +23,7 @@ if prompt := st.chat_input('Ask me about a property'):
         st.session_state.messages.append({'role':'user','content':prompt})
     with st.spinner("Analyzing UAE market data...."):
         user_input={'user_query':prompt}
-        final_state=real_estate_agent.invoke(user_input)
+        final_state=real_estate_agent.invoke(user_input,{'configurable':{"thread_id":st.session_state.thread_id}})
         ai_response=final_state['final_response']
     with st.chat_message('assistant'):
         st.markdown(ai_response)
